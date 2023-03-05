7754cf89e91d69845104feeb562f45010e24c145
3019244337-Liu Gelun <476823412@qq.com>
2023-03-05 21:27:41 +0800

    软件界面WPF代码编写，实现按钮按下变色功能

---
+++ Software/CANController/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CANController
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //按钮按下改变颜色
+        int tag = 1;
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (tag == 0)
+            {
+                btn.Background = new SolidColorBrush(Colors.White);
+                tag = 1;
+            }
+            else
+            {
+                btn.Background = new SolidColorBrush(Colors.LightSkyBlue);
+                tag = 0;
+            }
+        }
+    }
+}
