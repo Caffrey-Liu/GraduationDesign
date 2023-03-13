96f5bcfe68eb9e23c8baafeaf7218c5782d199e7
3019244337-Liu Gelun <476823412@qq.com>
2023-03-13 17:48:30 +0800

    对control.dll重新封装，实现CAN分析仪连接和启动

---
+++ Software/CANController/CANtest.cs
@@ -0,0 +1,30 @@
+﻿using CAN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CANController
+{
+    internal class CANtest
+    {
+        static void Main(string[] args)
+        {
+            CANhelper can = new CANhelper();
+            can.CanSetting.SetCAN((uint)0, //deviceIndex 设备索引号
+                                  (byte)0, //canIndex CAN的路索引号
+                                  (byte)CanFilterType.DualFilter, //filterType 过滤类型
+                                  (byte)CanMode.NormalMode, //canMode CAN模式
+                                  "00000000", //canCode 过滤码
+                                  "FFFFFFFF", //canMask 掩码
+                                  "4F", //时间高位
+                                  "2F"); //时间低位,默认 4F 2F
+            can.ConnectCANDevice(); //连接CAN设备
+            Console.WriteLine("连接成功");
+            can.StartCAN(); //启动CAN设备
+            Console.WriteLine("启动成功");
+            
+        }
+    }
+}
